41c76fe7ed781a9f1913235e0105c79a1b9d5029
Jozef <jozefw@sbcglobal.net>
2014-11-22 15:13:16 -0800

    first commit

---
+++ format.js
@@ -0,0 +1,7 @@
+function DivisionStringified(num1,num2) { 
+var solution = Math.round(num1 / num2);
+  solution = (solution.toLocaleString());
+  console.log(solution);
+  return solution; 
+         
+}
